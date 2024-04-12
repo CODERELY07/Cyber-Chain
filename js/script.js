412ca1d1c2ee6af5ac20a11c36e01bd1fc033129
Mark Ely Calipjo <calipjo.markely@gmail.com>
2024-04-12 09:38:28 +0800

    add-index-image

--- js/script.js
+++ js/script.js
@@ -13,12 +13,14 @@ bar.addEventListener('click', function() {
         }, 300); 
         icon.classList.remove('fa-x');
         icon.classList.add('fa-bars');
+        document.body.style.overflow = "auto";
         
     } else {
         nav.style.opacity = "1";
         search.style.opacity = "1";
         icon.classList.remove('fa-bars');
         icon.classList.add('fa-x');
+        document.body.style.overflow = "hidden";
     }
 });
 window.addEventListener('resize', function() {
